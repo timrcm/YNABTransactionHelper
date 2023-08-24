14a2c92bbfcff1a074b005eba1a2aebbf85b7434
timrcm <tim@timrcm.com>
2023-08-24 15:00:47 -0600

    dump json responses, testing with posting to fastmail echo method

--- fastmail.py
+++ fastmail.py
@@ -1,3 +1,4 @@
+import json
 from os import getenv
 import requests
 
@@ -5,12 +6,31 @@ import requests
 class Fastmail:
     def __init__(self):
         self.token = getenv('FASTMAIL_TOKEN')
-        self.endpoint = 'https://api.fastmail.com/jmap/session'
+        self.endpoint = 'https://api.fastmail.com/jmap'
         self.headers = {
-            'Authorization': f'Bearer {self.token}'
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/json',
         }
 
     def get_account(self):
-        response = requests.get(url=self.endpoint,
+        response = requests.get(url=f'{self.endpoint}/session',
                                 headers=self.headers)
+        return response.json()
+
+    def get_mail(self):
+        params = {
+            'using': ['urn:ietf:params:jmap:core', 'urn:ietf:params:jmap:mail'],
+            'methodCalls': [
+                ['Core/echo', {
+                    'accountId': getenv('FASTMAIL_ACCOUNT_ID'),
+                    'hello': True,
+                    'blarg': 5
+                },
+                 'a']
+            ]}
+        response = requests.post(url=f'{self.endpoint}/api',
+                                 headers=self.headers,
+                                 data=json.dumps(params))
+        print(response)
+        print(response.text)
         return response.json()
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import json
 from fastmail import Fastmail
 from ynab import YNAB
 
 if __name__ == '__main__':
-    ynab = YNAB()
     fastmail = Fastmail()
-    print(ynab.get_budgets())
-    print(fastmail.get_account())
+    ynab = YNAB()
+    # with open('fastmail.json', 'w') as f:
+    #     f.write(json.dumps(fastmail.get_account(), indent=2))
+    # with open('ynab.json', 'w') as f:
+    #     f.write(json.dumps(ynab.get_budgets(), indent=2))
+
+    with open('mail.json', 'w') as f:
+        f.write(json.dumps(fastmail.get_mail(), indent=2))
